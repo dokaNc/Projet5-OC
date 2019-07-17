5d4fd6646176db6941de698c30bf1ef441704103
Dogukan Cirpan <cirpan.dogukan5959@gmail.com>
2019-07-17 03:04:35 +0200

    add routing & home page

---
+++ src/Controllers/RouterController.php
@@ -0,0 +1,40 @@
+<?php
+
+    namespace Controller;
+
+    use Twig\Environment;
+    use Twig\Loader\FilesystemLoader;
+
+    class RouterController
+    {
+        const DEFAULT_PATH = 'Controller\\';
+        const DEFAULT_CONTROLLER  = 'IndexController';
+
+        public $twig;
+
+        public function __construct() {
+            $this->twigLoader();
+        }
+
+        public function twigLoader() {
+            $loader = new FilesystemLoader('../src/Views/');
+            $this->twig = new Environment($loader, [
+                'cache' => false,
+            ]);
+        }
+
+        public function route() {
+            $page = ucfirst(filter_input(INPUT_GET, 'page'));
+            if (!$page) {
+                $controller = new IndexController();
+                $controller->defaultAction($this->twig);
+            } else if (class_exists('Controller\\'.$page.'Controller')) {
+                $namespace = "\Controller\\".$page."Controller";
+                $controller = new $namespace;
+                $controller->defaultAction($this->twig);
+            } else {
+                $controller = new HandlerController();
+                $controller->defaultAction($this->twig);
+            }
+        }
+    }
