f6c8807271b3e6fcaa23063a847f78c4d64682f9
Dogukan <cirpan.dogukan5959@gmail.com>
2019-08-22 01:21:56 +0200

    connect to Database

--- src/Models/Manager.php
+++ src/Models/Manager.php
@@ -1,17 +1,24 @@
 <?php
 
-    namespace App\Model;
+namespace App\Model;
 
-USE PDO;
+use PDO;
 
-    abstract class Manager
+abstract class Manager
 {
 
-    public function connectDTB()
-    {
-        require_once ('../../config/config.php');
+    static protected $dtb = null;
 
-        $dtb = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $dtb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    public function connectDB()
+    {
+        require_once ('../config/config.php');
+
+        if ((self::$dtb) === null) {
+            $dtb = new PDO('mysql:host=' . HOST_NAME . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PWD);
+            $dtb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            self::$dtb = $dtb;
+        }
+        return self::$dtb;
     }
 }
