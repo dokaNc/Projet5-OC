8056980a65f9c68e559f9cff28dba646b7eb1bcf
Dogukan <cirpan.dogukan5959@gmail.com>
2019-08-28 16:56:39 +0200

    add alert message (error & success)

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -4,28 +4,55 @@
 
     use App\Model\UserManager;
 
-    class RegisterController {
+    class UserController {
+
+        protected $twig;
         
-        public function __construct() {
+        public function __construct($twig) {
+            $this->twig = $twig;
         }
 
-        public function defaultAction($twig) {
+        public function registerAction() {
             $register = new UserManager();
 
-            if (isset($_POST['username'])){
+            $success = false;
+            $errors = array();
 
-                    $data['username'] = filter_input(INPUT_POST, 'username');
-                    $data['email'] = filter_input(INPUT_POST, 'email');
-                    $data['password'] = filter_input(INPUT_POST, 'password');
-                    $data['password2'] = filter_input(INPUT_POST, 'passwordconfirm');
-                    $data['firstname'] = filter_input(INPUT_POST, 'firstname');
-                    $data['lastname'] = filter_input(INPUT_POST, 'lastname');
-              
+            if (count(filter_input(INPUT_POST, 'submit')) === 1) {
 
-                $register->createUser($data);
+                $data = array(
+                    "username" => filter_input(INPUT_POST, 'username'),
+                    "email" => filter_input(INPUT_POST, 'email'),
+                    "password" => filter_input(INPUT_POST, 'password'),
+                    "password2" => filter_input(INPUT_POST, 'passwordconfirm'),
+                    "firstname" => filter_input(INPUT_POST, 'firstname'),
+                    "lastname" => filter_input(INPUT_POST, 'lastname')
+                );
 
+                if ($data['password'] != $data['password2']) {
+                    array_push($errors, 'Les mots de passe ne sont pas identiques.');
+                }
+
+                if (strlen($data['username']) < 5) {
+                    array_push($errors, 'Le pseudonyme doit faire un minimum de 5 caractères.');
+                }
+
+                if (count($errors) == 0 && count(array_filter($data)) === 6) {
+                    // tous les champs sont remplis.
+                    $success = true;
+                    $register->createUser($data);
+                }
             }
             
-            echo $twig->render('user/register.twig');
+            return $this->twig->render('user/register.twig', [
+                'success' => $success,
+                'errors' => $errors
+            ]);
+        }
+
+        public function defaultAction() {
+            //return $this->twig->render('user/index.twig');
+            header("Status: 301 Moved Permanently", false, 301);
+            header("Location: index.php");
         }
     }
