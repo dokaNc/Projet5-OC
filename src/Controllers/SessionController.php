6b48b78f320b707f7474c94e9f42712d48e5372f
Dogukan <cirpan.dogukan5959@gmail.com>
2019-09-12 14:41:43 +0200

    init SessionController

---
+++ src/Controllers/SessionController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+class SessionController
+{
+
+    private $session;
+    private $user;
+
+    public function __construct()
+    {
+        $this->session = filter_var_array($_SESSION);
+        if (isset($this->session['user'])) {
+            $this->user = $this->session['user'];
+        }
+    }
+
+    public function createSession(int $idy, string $username, string $email, string $status)
+    {
+        $_SESSION['user'] = [
+            'id' => $idy,
+            'username' => $username,
+            'email' => $email,
+            'status' => $status,
+        ];
+    }
+
+    public function destroySession()
+    {
+        $_SESSION['user'] = [];
+        session_destroy();
+    }
+
+    public function isLogged()
+    {
+        if (array_key_exists('user', $this->session)) {
+            if (!empty($this->user)) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function checkStatus(string $info)
+    {
+        if ($info === 'admin') {
+
+            return true;
+        }
+        return false;
+    }
+
+}
