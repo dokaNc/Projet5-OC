9f1c0c866dbe58296643824b1232b8fa09c6d5e5
Dogukan <cirpan.dogukan5959@gmail.com>
2019-09-12 14:41:02 +0200

    session_start added on index

--- public/index.php
+++ public/index.php
@@ -4,5 +4,9 @@ use App\Controller\FrontController;
 
 require_once '../vendor/autoload.php';
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 $frontController = new FrontController();
 $frontController->run();
